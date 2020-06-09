1db058a3abdbd7a20dc1287fa89dc7117608335b
David Rodriguez <alu0101120645@ull.edu.es>
2020-06-09 19:53:27 +0100

    Añadiendo carousel

--- index.js
+++ index.js
@@ -2,3 +2,21 @@ document.addEventListener('DOMContentLoaded', function() {
   var elems = document.querySelectorAll('.sidenav');
   var instances = M.Sidenav.init(elems);
 });
+
+let carouselElems = document.querySelectorAll(".carousel");
+M.Carousel.init(carouselElems, {
+  duration: 0,
+  indicators: true,
+  fullWidth: true,
+});
+
+carouselElems.forEach((carouselElem) => {
+  let instance = M.Carousel.getInstance(carouselElem);
+  let nextButton = carouselElem.querySelector("#Next-btn");
+
+  nextButton.onclick = () => {
+    instance.next();
+  };
+});
+
+
